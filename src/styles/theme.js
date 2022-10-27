c2e37726eff17fff3df574e691f8c05c89dcdcc3
Bong <2021bongwonny@gmail.com>
2022-10-28 02:50:20 +0900

    🚀Modify : dist 재추가

--- src/styles/theme.js
+++
@@ -1,9 +0,0 @@
-export const contentFont = 'IM_Hyemin-Bold, sans-serif';
-
-export const titleFont = 'Cafe24Ssurround, sans-serif';
-
-export const containerShadow = '6px 6px 20px rgba(0, 0, 0, 0.2)';
-
-export const btnShadow = '6px 6px 20px rgba(0, 0, 0, 0.3)';
-
-export const activeColor = '#333';
