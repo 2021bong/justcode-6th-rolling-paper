e75c92574c2bd944dfc87d288e04e2852cf1c481
Bong <2021bongwonny@gmail.com>
2022-10-31 00:15:29 +0900

    💄Add : randomColors 색상 추가 - 색상 추가 및 기존 컬러 변경

--- src/utils/color.js
+++ src/utils/color.js
@@ -4,15 +4,18 @@ export const randomColorsLeft = ['#FF645E', '#D340DE', '#535BF5'];
 // 하늘 초록 핑크
 export const randomColorsRight = ['#53B8E0', '#1CCF55', '#E06DC1'];
 
-// 빨강 보라 파랑 하늘 초록 핑크 노랑 연보라 주황
+// 빨간코랄 연한보라 연한남색 밝은하늘 형광초록 중채도분홍 개나리노랑 푸른연보라 진한노랑 연한주황 중채도민트 붉은분홍 쿨그레이
 export const randomColors = [
   '#FF645E',
-  '#D340DE',
-  '#535BF5',
-  '#53B8E0',
+  '#9D4AD9',
+  '#5C64ED',
+  '#30ABDB',
   '#1CCF55',
   '#E06DC1',
   '#FCBB3A',
   '#8E7FF5',
-  '#E69640',
+  '#E67D27',
+  '#0BBFAA',
+  '#EB4473',
+  '#A6AABD',
 ];
